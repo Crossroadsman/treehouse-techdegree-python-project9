1cc2f28def1becfff3ebe301ae467f103bcad7a8
CrossroadsMan <crossroadsman@gmail.com>
2019-08-26 23:07:52 -0600

    Fix expiration_date on Menu model

--- improve_django_v3/menu/models.py
+++ improve_django_v3/menu/models.py
@@ -1,14 +1,25 @@
+import datetime
+
 from django.db import models
 from django.utils import timezone
+
+
+def set_expiry_date():
+    now = datetime.datetime.now()
+    naive_future = datetime.datetime(now.year + 2, now.month, now.day)
+    aware_future = timezone.make_aware(naive_future, is_dst=True)
+    return aware_future
 
 
 class Menu(models.Model):
     season = models.CharField(max_length=20)
     items = models.ManyToManyField('Item', related_name='items')
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    expiration_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    # We don't want to allow blank/null expiration dates as the application
+    # is designed to be able to perform comparisons on expiration_date values.
+    # We'll set a reasonable default of 2 years from now
+    expiration_date = models.DateTimeField(default=set_expiry_date)
 
     def __str__(self):
         return self.season
