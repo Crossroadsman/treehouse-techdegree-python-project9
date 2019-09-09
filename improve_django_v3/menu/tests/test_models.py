4247cc096805d84e04639170f69ab2591743e106
CrossroadsMan <crossroadsman@gmail.com>
2019-09-09 16:49:16 -0600

    Add additional model tests

--- improve_django_v3/menu/tests/test_models.py
+++ improve_django_v3/menu/tests/test_models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from .base import MenuTestCase, VALID_INGREDIENT_DATA
 
 from menu.models import Menu, Item, Ingredient
@@ -23,10 +25,39 @@ class MenuModelTests(MenuTestCase):
         self.assertEqual(expected_items, test_items)
 
     def test_create_expiration_date_is_correctly_generated(self):
-        pass
+        created_date = self.test_menu.created_date
+        expected_expiry = created_date + datetime.timedelta(days=(365 * 2))
+
+        almost_equal_delta = datetime.timedelta(seconds=10)
+
+        self.assertAlmostEqual(
+            expected_expiry,
+            self.test_menu.expiration_date,
+            delta=almost_equal_delta
+        )
 
     def test_meta_models_are_correctly_ordered(self):
-        pass
+        items = self.test_menu.items.all()
+        self.create_valid_menu(
+            items=items,
+            season='early 2020',
+        )
+        test_menu_3 = self.create_valid_menu(
+            items=items,
+            season='beginning 2018',
+        )
+        test_menu_3.expiration_date = self.test_menu.expiration_date
+        test_menu_3.save()
+
+        # first: beginning 2018 (same expiry as fall 2019 but season comes
+        #        first alphanetically);
+        # second: fall 2019
+        # third: early 2020 (expiration date comes after the other two)
+        expected_order = ['beginning 2018', 'fall 2019', 'early 2020']
+
+        results = list(Menu.objects.all().values_list('season', flat=True))
+
+        self.assertEqual(expected_order, results)
 
 
 class ItemModelTests(MenuTestCase):
